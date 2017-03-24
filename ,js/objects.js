5a46cfe19c78a85f6f00bd578e57483c7f1e5ba0
KingFish <yy@scoutshalle.be>
2017-03-24 13:07:30 +0100

    Object_Basic aangemaakt

---
+++ ,js/objects.js
@@ -0,0 +1,20 @@
+function Dinge(column, row, bomb, flagged, src){
+    //this.type
+    this.column = column,
+    this.row = row,
+    this.bomb = bomb || false,
+    this.flagged = false,
+    this.src = src
+}
+
+Dinge.prototype.clicked = function()
+    {
+        if (this.bomb)
+            {
+
+            }
+    }
+Dinge.prototype.rightclicked = function()
+    {
+        this.flagged = !this.flagged
+    }
