2ef7dd45c71a70f7a9403797a279ddc5e3ae88d3
KingFish <yy@scoutshalle.be>
2017-03-24 14:51:01 +0100

    Test + object

--- js/objects.js
+++ js/objects.js
@@ -12,10 +12,28 @@ Dinge.prototype.clicked = function()
                 return false;
             }
         else{
-
+            {
+                var count = 0;
+                for (var i = -1; i < 3; i++)
+                    {
+                         for (var p = -1; p < 3; p++)
+                            {
+                                    console.log(i+''+p);             
+                            }
+                    }
+            }
+            
         }
     }
 Dinge.prototype.rightclicked = function()
     {
         this.flagged = !this.flagged
     }
+
+function Board(cols, rows, bombs){
+    this.cols = cols,
+    this.rows = rows,
+    this.bombs = bombs,
+    this.create = function(){   
+    }
+}
