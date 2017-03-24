4881f5bb8c9a93961cf7986b6640be13f5ed89b4
KingFish <yy@scoutshalle.be>
2017-03-24 15:56:59 +0100

    hoeraz

--- js/objects.js
+++ js/objects.js
@@ -3,27 +3,30 @@ function Dinge(column, row, bomb){
     this.column = column,
     this.row = row,
     this.bomb = bomb || false,
-    this.flagged = false
+    this.flagged = false,
+    this.count = 0;
 }
 
-Dinge.prototype.clicked = function()
+Dinge.prototype.clicked = function(rij, kolom, field)
     {
-        if (this.bomb){
+        if (this.bomb)
+            {
                 return false;
             }
-        else{
-            {
-                var count = 0;
-                for (var i = -1; i < 3; i++)
+        else
+            {       //Grootste van deze rij-1 en 0          //Kleinste van lengte v.d row en deze rij + 1
+               for (var i = Math.max(this.row-1, 0); i <= Math.min(this.row+1, rij.length); i++)//Rij doorlopen
                     {
-                         for (var p = -1; p < 3; p++)
+                        for (var p= Math.max(this.column-1, 0); p <= Math.min(this.column, kolom.length); p++)
                             {
-                                    console.log(i+''+p);             
+                                if (field[i][p].bomb)
+                                    {
+                                        this.count++
+                                    }
                             }
                     }
+                return this.count;
             }
-            
-        }
     }
 Dinge.prototype.rightclicked = function()
     {
@@ -34,6 +37,15 @@ function Board(cols, rows, bombs){
     this.cols = cols,
     this.rows = rows,
     this.bombs = bombs,
-    this.create = function(){   
+    this.field = new Array(this.cols);
+    this.create = function(){
+        for(var i = 0; i < this.rows; i++)
+            {
+                this.field[i] = new Array(this.rows);
+            }
+        console.log(this.field);
     }
 }
+
+var b = new Board(10, 10, 10);
+b.create();
