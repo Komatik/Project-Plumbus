8457be867a30482ff2e36a0b9d4eac3d1aa18414
KingFish <yy@scoutshalle.be>
2017-03-29 13:11:49 +0200

    pug2

---
+++ Pugatronic/js/waves.js
@@ -0,0 +1,27 @@
+window.onload = function()
+    {
+        var ocean = document.getElementById("een"),
+            waveWidth = 10,
+            waveCount = Math.floor(window.innerWidth/waveWidth),
+            docFrag = document.createDocumentFragment();
+
+        for(var i = 0; i < waveCount; i++)
+            {
+                var wave = document.createElement("div");
+                wave.className += " wave";
+                docFrag.appendChild(wave);
+                wave.style.left = i * waveWidth + "px";
+                wave.style.webkitAnimationDelay = (i/100) + "s";
+            }
+
+        for(var p = 0; p < waveCount; p++)
+            {
+                var wave2 = document.createElement("div");
+                wave2.className += " wavetwee";
+                docFrag.appendChild(wave2);
+                wave2.style.left = p * waveWidth + "px";
+                wave2.style.webkitAnimationDelay = (p/100) + "s";
+            }
+
+        ocean.appendChild(docFrag);
+    }
