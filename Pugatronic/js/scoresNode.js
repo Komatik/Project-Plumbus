b67f2af5b741e7e8b52ccfc15ae46e2d1ed98d43
KingFish <yy@scoutshalle.be>
2017-04-04 16:08:27 +0200

    ScoreServer_v1

--- Pugatronic/js/scoresNode.js
+++ Pugatronic/js/scoresNode.js
@@ -4,20 +4,23 @@ const url = "ds145750.mlab.com:45750/plumbus";
 
 var express = require('express');
 var mongo = require('mongodb').MongoClient;
-var bodyParser = require('body-parser');
 var server = express();
 
-server.use(bodyParser.urlencoded({ extended: true }));
-server.all('/name/:name', function(req, res){
-});
-server.all('/top/naam/:naam', function (req, res){
+server.all('/top/naam/:naam', function (req, res){ //Roep alle scores bij naam op
     mongo.connect('mongodb://'+ url, function(err, db){
         if (!err){
             db.authenticate(user, pass);
             db.collection('scores').find(
                 {'naam':req.params['naam']}
-                ).toArray(function(err, scores){//Alle scores in array pushe
-                    res.send(JSON.stringify(scores));//Array jsonifen en terugsturen
+                ).toArray(function(errFind, scores){//Alle scores in array pushe
+                    if (!errFind)
+                        {
+                            res.send(JSON.stringify(scores));//Array jsonifen en terugsturen
+                        }
+                    else{
+                        res.setHeader('status', 500);
+                        res.send();
+                    }
             })
         }
         else{
@@ -26,18 +29,48 @@ server.all('/top/naam/:naam', function (req, res){
         }
     })
 });
-server.all('/top/diff/:row/:col/:bombs', function (req, res){
+
+server.all('/configs/', function(req, res){ //Roep alle configuraties en bijhorende scorelijsten op
+    mongo.connect('mongodb://'+ url, function(err, db){
+        if (!err){
+            db.authenticate(user, pass);
+            db.collection('scores').group(
+                ['col', 'row', 'bombs'],
+                {},
+                {"scores":[]},
+                "function(cur, result){result.scores.push({naam:cur.naam, tijd:cur.tijd})}",
+                function(errAggs, docs){
+                    if (!errAggs)
+                        {
+                            res.send(docs);
+                        }
+                    else{
+                        res.send(errAggs);
+                    }
+                })
+        }else{
+            res.setHeader('status', 500);
+            res.send();
+        }
+})})
+
+server.all('/top/diff/:row/:col/:bombs', function (req, res){ //Roep de lijst per configuratie op
    mongo.connect('mongodb://'+ url, function(err, db){
         if (!err){
             db.authenticate(user, pass);
-            var collection= db.collection('scores');
-            collection.find(
+            var collec = db.collection('scores');
+            collec.find({
+                "col":parseInt(req.params.col), 
+                "row":parseInt(req.params.row), 
+                "bombs":parseInt(req.params.bombs)}).toArray(function (errCol, given)
                 {
-                    'col':req.params.col, 
-                    'row':req.params.row, 
-                    'bombs':req.params.bombs}
-                ).toArray(function(errCol, given){
-                  res.send(given);
+                  if (!errCol)
+                    {
+                        res.send(given);
+                    }
+                    else{
+                        res.send(errCol);
+                    }
                 })
         }
         else{
@@ -46,7 +79,7 @@ server.all('/top/diff/:row/:col/:bombs', function (req, res){
         }
     })
 });
-server.all('/invoegen/:naam/:tijd/:row/:col/:bombs', function(req,res){
+server.all('/invoegen/:naam/:tijd/:row/:col/:bombs', function(req,res){ //Nieuwe score toevoegen
     mongo.connect('mongodb://'+url, function(err, db){
         if (!err){
             db.authenticate(user, pass);
