e422a6fe30d6f9446a6a06e38b1b2daebb14749b
samir <grovile@gmail.com>
2017-04-05 11:04:26 +0200

    e

--- Pugatronic/js/bubble.js
+++ Pugatronic/js/bubble.js
@@ -1,124 +1,63 @@
 'use strict';
-/*
-    * GUIDE 
-    *   Scene Obj
-    *   Bubble obj 
-    * 
-    ====  Scene ====
 
-     1) first create a new screen and set the 'id' of your canavs as parameter
-        for your scene. You can also define a width and height but is not necessary.
-
-            examp: var foo = new Scene( 'myCanvas', width, height );
-
-     2) To create a scene use createScene() if you leave the function parameter empty, 
-        the scene will use your default values for hight and width if you set it to 'true' the scene will be fullScreen.
-
-            examp: foo.createScene() // default values for hight and width 
-               or: foo.createScene( true ) // fullScreen and set to absolute
-     
+var canvas = document.getElementById('boil');
+var ctx = canvas.getContext('2d');
 
 
-      ====  Bubble ====
-*/
+// temporary settings
+canvas.style.position = 'absolute';
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight * 3;
 
-var windowView = document.getElementById( 'background' )
-var canvas = new Scene( null, windowView.offsetWidth, windowView.offsetHeight  )
+// js obj
 
-// ========== Setup  ==========
-
-
-
-window.onload = function (){
-    canvas.createScene();
-
-    // Xpos, Ypos, Straal, Widthstroke, Color
-    var bube = new Bubble ( canvas.ctx , 100, 100 );
-    bube.drawBubble()
-
-};
-
-// ========== Function ==========
-    var bubbleCollection = [];
-    
-    function update(){
-        canvas.ctx.clearRect( 0, 0, canvas.width, canvas.height );
-
-        var bub = new Bubble ( canvas.ctx , 100, 100 );
-        bub.drawBubble()
-
-    };
-
-    var bubbleTimer = setInterval( update,  100 )
-
-
-// ========== Event's ==========
-
-
-
-
-// ========== Scene Obj ==========
-
-function Scene( id, w, h ){
-    if( typeof id !== 'string' ){
-        let canvas =  document.getElementById('background').insertBefore( document.createElement( 'canvas' ), document.getElementById('background').firstChild )
-        this.screen = canvas;
-    } else {
-        this.screen = document.getElementById( id ) || document.getElementsByTagName( 'canvas' )[ 0 ];
-    }
-    this.ctx = this.screen.getContext( '2d' );
-    this.width = w || this.screen.width;
-    this.height = h || this.screen.height;
-    this.position = 'absolute';
- }
-
-Scene.prototype.createScene = function ( fullScreen ){
-
-    if( fullScreen == true ){
-
-        this.screen.style.position = this.position;
-        this.screen.width = window.innerWidth;
-        this.screen.height = window.innerHeight;
-        // change defaults to new data
-        this.width = window.innerWidth;
-        this.height = window.innerHeight;
-
-    } else{
-
-        this.screen.width = this.width;
-        this.screen.height = this.height;
-
-    }
-}
-
-// ========== Bubble Obj ==========
-
-function Bubble( ctx, x, y, s, w, c ){
-
-    this.ctx = ctx;
+function Bubble( x, y, s ) {
     this.x = x;
     this.y = y;
-    this.s = s || window.innerWidth / 30;
-    this.w = w || 3; 
-    this.c = c || '#fff';
-
-}
-
-Bubble.prototype.drawBubble = function(){
-
-    this.ctx.beginPath();
-    this.ctx.arc( this.x, this.y, this.s, 0, 2 * Math.PI );
-    this.ctx.strokeStyle = this.c
-    this.ctx.lineWidth = this.w;
-    this.ctx.stroke();
-    this.ctx.closePath();
-    console.log('bubble')
-}
-
-Bubble.prototype.behaviour = function(){
-
-}
-
-Bubble.prototype.move = function(){
+    this.s = s;
+    var self = this;
     
+    setInterval( function(){
+        if (self.y > 0){
+            self.y--;
+        }
+        else{
+            self.s = Math.floor( Math.random() * 50 );
+            self.y = canvas.offsetHeight + 500;
+            self.x = Math.floor( Math.random() * canvas.offsetWidth + 1 )
+        }
+        update();
+    }, Math.floor(Math.random() * ( self.s > 100 ? 1000  : 50  ) ));
+    
+}
+
+Bubble.prototype.drawBubble = function () {
+
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.s, 0, 2 * Math.PI);
+    //ctx.fillStyle = 'rgba(255, 255, 255, 0.3)'
+    ctx.strokeStyle = '#fff'
+    ctx.stroke();
+    ctx.closePath();
+
+} // end drawBubble
+
+var bubbles = []
+
+for( var i = 0; i < 15; i++ ){
+    var rx = Math.floor( Math.random() * canvas.offsetWidth + 1 );
+    var ry =  Math.floor( canvas.offsetHeight + (Math.random()* 900) );
+    var rs = Math.floor( Math.random() * 50 )
+    bubbles.push(
+        new Bubble( rx, ry, rs )
+    )
+}
+
+// update function 
+function update() {
+    ctx.clearRect( 0, 0, canvas.width, canvas.height); // clears canvas
+
+    for (var i = 0; i < bubbles.length; i++) {   
+        bubbles[i].drawBubble();
+    }
 }
